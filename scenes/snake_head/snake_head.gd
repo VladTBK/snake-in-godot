15c2954a1b8c37264b03bccd57cc74cf33d3f38c
VladTBK <most_loginVlad.TBK@proton.me>
2024-01-29 20:07:33 +0200

    fixed godot, added collision with apple

--- scenes/snake_head/snake_head.gd
+++ scenes/snake_head/snake_head.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-const SNAKE_SPEED = 1
+var snake_speed = 1
+var score = 0
 var curr_dir = Vector2(1, 0)
 
 
@@ -15,7 +16,7 @@ func _process(delta):
 	if new_dir != Vector2.ZERO and abs(new_dir) != Vector2.ONE:
 		curr_dir = new_dir
 
-	global_position += SNAKE_SPEED * curr_dir
+	global_position += snake_speed * curr_dir
 	rotation = curr_dir.angle()
 
 
@@ -23,3 +24,10 @@ func get_dir():
 	var x_dir = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_dir = -Input.get_action_strength("move_up") + Input.get_action_strength("move_down")
 	return Vector2(x_dir, y_dir)
+
+
+func _on_area_2d_area_entered(area):
+	if area.get_parent().is_in_group("apple"):
+		area.get_parent().queue_free()
+		score += 1
+		snake_speed += 0.5

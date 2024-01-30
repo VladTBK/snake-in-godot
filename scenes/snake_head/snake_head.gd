948e2441445e79cb3e1c94219aa6b06d9cde2db8
VladTBK <most_loginVlad.TBK@proton.me>
2024-01-30 17:16:39 +0200

    couldn't get snake without proper grid

--- scenes/snake_head/snake_head.gd
+++ scenes/snake_head/snake_head.gd
@@ -1,13 +1,15 @@
-extends Node2D
+extends CharacterBody2D
 
-var snake_speed = 1
+var snake_speed = 100
 var score = 0
+var size = 3
 var curr_dir = Vector2(1, 0)
+var part_size = 25
+@export var body: PackedScene
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass  # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,8 +17,9 @@ func _process(delta):
 	var new_dir = get_dir()
 	if new_dir != Vector2.ZERO and abs(new_dir) != Vector2.ONE:
 		curr_dir = new_dir
-
-	global_position += snake_speed * curr_dir
+	velocity = curr_dir * snake_speed
+	var temp_old_pos = global_position
+	move_and_slide()
 	rotation = curr_dir.angle()
 
 
@@ -29,7 +32,9 @@ func get_dir():
 func _on_area_2d_area_entered(area):
 	if area.get_parent().is_in_group("apple"):
 		score += 1
-		snake_speed += 0.5
+		snake_speed += 10
+		# var apple_sizex = area.get_parent().global_transform.x
+		# var apple_sizey = area.get_parent().get_node("Sprite2D").global_transform.y
 		var x_pos = randf_range(0, get_viewport().size.x)
 		var y_pos = randf_range(0, get_viewport().size.y)
 		area.get_parent().global_position = Vector2(x_pos, y_pos)
